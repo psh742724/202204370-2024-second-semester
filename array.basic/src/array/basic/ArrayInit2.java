136807d85bb310955d23860eb723bc0c1e8b0669
psh74 <psh74@10.10.12.199>
2024-10-08 10:19:43 +0900

    commit - 24.10.8

---
+++ array.basic/src/array/basic/ArrayInit2.java
@@ -0,0 +1,15 @@
+package array.basic;
+
+public class ArrayInit2 {
+
+	public static void main(String[] args) {
+		int score[];
+		
+		score = new int[] {2, 4, 7};
+		System.out.printf("%d %d %d %n", score[0], score[1], score[2]);
+
+		score = new int[] {23, 46, 78};
+		System.out.printf("%d %d %d %n", score[0], score[1], score[2]);
+	}
+
+}
