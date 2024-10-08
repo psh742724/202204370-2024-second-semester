0edf401c74a5c57e38d6c3a269974caf4b5aadf4
psh74 <psh74@10.10.12.199>
2024-10-08 10:48:37 +0900

    commit - 24.10.8

--- array.basic/src/array/basic/Arraycopy.java
+++ array.basic/src/array/basic/Arraycopy.java
@@ -7,19 +7,26 @@ public class Arraycopy {
 		int[] copyTo = {10, 20, 30, 40, 50, 60, 70, 80};
 		System.arraycopy(copyFrom, 4, copyTo, 1, 3);
 		
-		for (int i = 0; i < copyFrom.length; i++) {
+		/*
+		for (int i = 0; i < copyFrom.length; i++) { // 코드 #1 - 원본
 			System.out.print(copyFrom[i] + " ");
+		}
+		System.out.println();
+		*/
+		
+		for (int value : copyFrom) {
+			System.out.print(value + " "); // 코드 #1 - 축약본
 		}
 		System.out.println();
 		
 		for (int i = 0, value; i < copyTo.length; i++) {
 			value = copyTo[i];
-			System.out.print(value + " ");
+			System.out.print(value + " "); // 원본
 		}
-
 		
-		/* for (int value : copyTo) {
-			System.out.print(value + " ");
+		/* 
+		for (int value : copyTo) {
+			System.out.print(value + " "); // 축약본
 		}
 		*/
 	}
