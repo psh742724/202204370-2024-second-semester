b8531e17035a529b02c4b38fb72f24f3ef31c3fd
psh74 <psh74@10.10.17.236>
2024-10-17 15:30:31 +0900

    commit - 24.10.17

---
+++ array.basic/src/array/basic/UsingArray.java
@@ -0,0 +1,15 @@
+package array.basic;
+
+public class UsingArray {
+
+	public static void main(String[] args) {
+		String name[] = {"C++", "Java", "C#"};
+		
+		for (String str : name) {
+			System.out.print(str + " ");
+		}
+		System.out.println();
+
+	}
+
+}
