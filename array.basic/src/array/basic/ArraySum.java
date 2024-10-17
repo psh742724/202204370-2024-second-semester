07916277f33553cd9951859cef83e43a533e6731
psh74 <psh74@10.10.17.236>
2024-10-17 16:17:14 +0900

    commit - 24.10.17

---
+++ array.basic/src/array/basic/ArraySum.java
@@ -0,0 +1,28 @@
+package array.basic;
+
+import java.util.Scanner;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("배열 크기 입력 > ");
+		int size = input.nextInt();
+		
+		int score[] = new int[size];
+		
+		System.out.print("성적 입력 > ");
+		for (int i = 0; i < score.length; i++) {
+			score[i] = input.nextInt();
+		}
+		
+		for (int value : score) {
+			System.out.print(value + " ");
+		}
+		
+		input.close();
+		
+
+	}
+
+}
--- array.basic/src/array/basic/UsingArray.java
+++ array.basic/src/array/basic/UsingArray.java
@@ -5,10 +5,26 @@ public class UsingArray {
 	public static void main(String[] args) {
 		String name[] = {"C++", "Java", "C#"};
 		
-		for (String str : name) {
+		/*
+		for (String str : name) { // 방법 1). for each 문
 			System.out.print(str + " ");
 		}
 		System.out.println();
+		*/
+		
+		for (int i = 0; i < name.length; i++) { // 방법 2). for 문
+			String str = name[i];
+			System.out.print(str + ", ");
+		}
+		
+		String data[][] = {{"홍", "길동"}, {"최", "경", "주"}, {"Tiger", "Woods"}};
+		
+		for (String ary[] : data) {
+			for (String value : ary) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}
 
 	}
 
