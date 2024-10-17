35373cc1b335457274b313f75a5f1529661090c9
psh74 <psh74@10.10.17.236>
2024-10-17 16:24:46 +0900

    command - 24.10.17

---
+++ array.basic/src/array/basic/Command.java
@@ -0,0 +1,14 @@
+package array.basic;
+
+public class Command {
+
+	public static void main(String[] args) {
+		System.out.println("명령행 인자 갯수 > " + args.length);
+		
+		for (String value : args) {
+			System.out.print(value + " ");
+		}
+
+	}
+
+}
