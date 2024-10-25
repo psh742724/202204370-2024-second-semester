90520ab12e0aa5b3efeee0fd4163fec642f99f78
psh74 <psh74@10.10.17.236>
2024-10-25 17:06:32 +0900

    commit - 24.10.25

--- obj.constructor/src/obj/defaultconstructor/Student.java
+++ obj.constructor/src/obj/defaultconstructor/Student.java
@@ -18,8 +18,14 @@ public class Student {
 	}
 
 	public static void main(String[] args) {
-		
+		Student lee = new Student("이승훈");
+		lee.print();
 
+		
+		//이름을 인자로 하는 생성자 구현 => 기본 생성자를 직접 구현해야 기본 생성자로 객체 생성 가능
+		Student kim = new Student();
+		kim.name = "김다빈";
+		kim.print();
 	}
 
 }
