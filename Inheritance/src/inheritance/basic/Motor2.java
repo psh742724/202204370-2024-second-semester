cdf1ff825cce351f9169a34a18d93f1b181e9a0e
psh74 <psh74@10.10.23.254>
2024-10-31 16:04:13 +0900

    commit - 24.10.31

---
+++ Inheritance/src/inheritance/basic/Motor2.java
@@ -0,0 +1,37 @@
+package inheritance.basic;
+
+public class Motor2 extends Vehicle {
+	public String name = "자동차";
+	public int displacement;
+	
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	
+	public void setSeater(int seater) {
+		this.seater = seater;
+	}
+	
+	public void setDisplacement(int displacement) {
+		this.displacement = displacement;
+	}
+
+	public void printInfo() {
+		System.out.print(super.name + ": " + this.name);
+		System.out.println(", 최대속도: " + maxSpeed + " km");
+		System.out.print("정원: " + seater + " 명");
+		System.out.println(", 배기량: " + displacement + " cc");
+	}
+	
+	public static void main(String[] args) {
+		Motor2 myCar = new Motor2();
+		
+		myCar.setMaxSpeed(300);
+		myCar.setSeater(2);
+		myCar.setDisplacement(3500);
+		
+		myCar.printInfo();
+
+	}
+
+}
---
+++ Inheritance/src/inheritance/basic/Vehicle.java
@@ -0,0 +1,8 @@
+package inheritance.basic;
+
+public class Vehicle {
+	public String name = "차량";
+	public double maxSpeed;
+	public int seater;
+
+}
