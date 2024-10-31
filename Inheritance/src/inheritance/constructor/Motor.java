c4bdf0b075fc3723fb07384f8f9be8e43963c50c
psh74 <psh74@10.10.23.254>
2024-10-31 16:28:56 +0900

    commit - 24.10.31

---
+++ Inheritance/src/inheritance/constructor/Motor.java
@@ -0,0 +1,32 @@
+package inheritance.constructor;
+
+public class Motor extends Vehicle {
+	public String name = "자동차";
+	public int displacement;
+	
+	public Motor() {
+		super(); // 생략 가능
+	}
+	
+	public Motor(double maxSpeed, int seater, int displacement) {
+		super(maxSpeed, seater);
+		/*
+		 this.maxSpeed = maxSpeed;
+		 this.seater = seater;
+		 */
+		this.displacement = displacement;
+	}
+	
+	public void printInfo() {
+		System.out.print(super.name + ": " + this.name);
+		System.out.println(", 최대속도: " + maxSpeed + " km");
+		System.out.print("정원: " + seater + " 명");
+		System.out.println(", 배기량: " + displacement + " cc");
+	}
+
+	public static void main(String[] args) {
+		Motor myCar = new Motor(300, 4, 5000);
+		myCar.printInfo();
+	}
+
+}
