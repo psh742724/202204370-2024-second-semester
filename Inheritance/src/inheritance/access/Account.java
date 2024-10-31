cd128b7fb3e2693467d1ccfffbb3d8506183f736
psh74 <psh74@10.10.23.254>
2024-10-31 19:00:09 +0900

    commit - 24.10.31

---
+++ Inheritance/src/inheritance/access/Account.java
@@ -0,0 +1,36 @@
+package inheritance.access;
+
+import java.util.Date;
+
+public class Account {
+	public String name;
+	protected Date open;
+	/* package */ int number;
+	private long balance;
+	
+	
+	//생성자 - 동일 패키지 및 하위 클레스에서 사용 가능
+	protected Account(String name, int number) {
+		this.name = name;
+		this.number = number;
+		this.open = new Date();
+	}
+	
+	
+	//일반 메소두
+	
+	//입금 메소드
+	public long deposit(long amount) {
+		return balance += amount;
+	}
+	//출 메소드
+	public long withdraw(long amount) {
+		return balance -= amount;
+	}
+	//계좌정보 및 잔액출력 메소드
+	public long checkBalance() {
+		System.out.println(name + ": 계좌번호 " + number + ", 잔액 " + balance);
+		return balance;
+	}
+	
+}
