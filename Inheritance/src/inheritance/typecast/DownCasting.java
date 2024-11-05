69b978c0e67b2dce2d734fecfbdafc521fb416f7
psh74 <psh74@10.10.9.112>
2024-11-05 17:44:43 +0900

    commit - 24.11.5

---
+++ Inheritance/src/inheritance/typecast/DownCasting.java
@@ -0,0 +1,21 @@
+package inheritance.typecast;
+
+public class DownCasting {
+
+	public static void main(String[] args) {
+		Person she = new Person("이소라", 2056432);
+		System.out.println(she.name + " " + she.number);
+		//Faculty f = she; -- 컴파일 오류
+		//Faculty f1 = (Faculty) she; -- 실행 오류
+		
+		Person p = new Staff("김상기", 1187543, "강서대학교", 3456);
+		//Staff s = p; -- 컴파일 오류
+		Staff s = (Staff) p;
+		s.division = "교학처";
+		System.out.print(p.name + " " + p.number + " ");
+		System.out.print(s.univ + " " + s.number + " ");
+		System.out.print(s.division);
+		
+	}
+
+}
