e41643199e00e262989132f85c32a3d563807d11
psh74 <psh74@10.10.17.192>
2024-11-26 10:19:44 +0900

    commit - 24.11.26

---
+++ java.lang/src/HashSetTest.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class HashSetTest {
+
+	public static void main(String[] args) {
+		HashSet<Integer> setA = new HashSet<Integer>();
+		HashSet<Integer> setB = new HashSet<Integer>();
+		
+		setA.add(3); setA.add(5); setA.add(7);
+		setA.add(8); setA.add(7); setA.add(9);
+		System.out.print("A = ");
+		print(setA);
+		System.out.println(" A = " + setA);
+
+		
+		setB.add(5); setB.add(3); setB.add(2);
+		System.out.print("B = ");
+		print(setB);
+		System.out.println(" B = " + setB);
+		
+		
+		
+		boolean isChanged = setA.removeAll(setB); // A - B
+		System.out.print("A - B = ");
+		if (isChanged) {
+			System.out.println(setA);
+		}
+		
+	}
+	
+	public static void print(HashSet<Integer> s) {
+		Iterator<Integer> i = s.iterator();
+		while (i.hasNext()) {
+			System.out.print(i.next() + " ");
+		}
+	}
+
+}
