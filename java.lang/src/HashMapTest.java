b5264e36c60d9d10eec1450faf4fea7b36dcdb50
psh74 <psh74@10.10.17.192>
2024-11-26 10:46:23 +0900

    commit - 24.11.26

---
+++ java.lang/src/HashMapTest.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+		
+		hm.put("대한민국", "서울");
+		hm.put("일본", "동경");
+		hm.put("중국", "북경");
+		hm.put("태국", "방콕");    hm.remove("태국");
+		hm.put("중국", "북경");
+		
+		System.out.print("키 : " + hm.keySet());
+		System.out.println(" 값 : " + hm.values());
+		
+		Iterator<String> keys = hm.keySet().iterator();
+		
+		while (keys.hasNext()) {
+			String key = keys.next();
+			String value = hm.get(key);
+			
+			System.out.println(key + ": " + value);
+		}
+
+	}
+
+}
