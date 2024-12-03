7b039033a5949ffe00b1317045939cf58a0e5300
psh74 <psh74@10.10.13.95>
2024-12-03 10:27:54 +0900

    commit - 24.12.3

---
+++ Exception/src/ExceptionType1.java
@@ -0,0 +1,9 @@
+public class ExceptionType1 {
+	static int num[];
+	
+	public static void main(String[] args) {
+		System.out.println(num.length);
+
+	}
+
+}
---
+++ Exception/src/ExceptionType2.java
@@ -0,0 +1,12 @@
+public class ExceptionType2 {
+
+	public static void main(String[] args) {
+		String str[] = {"안녕하세요!", "Hello!"};
+		
+		System.out.println(str[0]);
+		System.out.println(str[2]);
+		System.out.println(str[1]);
+
+	}
+
+}
